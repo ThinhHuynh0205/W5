33837d79c912dcfab3ac779b01eca42e549a9817
ThinhHuynh0205 <huynhthinh0205@gmail.com>
2023-03-10 23:26:00 +0700

    Initial commit

---
+++ app/src/main/java/com/android/w5/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.android.w5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/android/w5/Signup.kt
@@ -0,0 +1,16 @@
+package com.android.w5
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Signup.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Signup : Fragment(R.layout.fragment_signup) {
+
+}
